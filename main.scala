0348cd97a78e27943e034660422d0fd06e9fc209
Jakub Kozłowski <kubukoz@gmail.com>
2026-04-11 01:48:29 +0200

    Init

---
+++ main.scala
@@ -0,0 +1,13 @@
+//> using scala 3.7.0
+//> using options -no-indent
+
+//> using publish.computeVersion git:tag
+//> using publish.developers "kubukoz|Jakub Kozłowski|https://github.com/kubukoz"
+//> using publish.license Apache-2.0
+//> using publish.name named-tupled
+//> using publish.organization org.polyvariant
+//> using publish.repository central
+//> using publish.secretKey env:PGP_SECRET
+//> using publish.secretKeyPassword env:PGP_PASSPHRASE
+//> using publish.url https://github.com/polyvariant/named-tupled
+//> using publish.vcs github:polyvariant/named-tupled
